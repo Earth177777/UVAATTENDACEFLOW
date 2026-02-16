7705f53e7d91350dcc6b6d55d68223a9a0072ee0
Gilbert <gilbert177777@gmail.com>
2026-02-16 15:03:40 +0700

    Increase backend upload limit to 10MB

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -57,6 +57,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Routes
 app.use('/api', apiRoutes);
