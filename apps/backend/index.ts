fd1b2daa8edb14796656da362a0ccd231e9eecd8
Gilbert <gilbert177777@gmail.com>
2026-02-16 14:59:55 +0700

    Increase API rate limit to 1000

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -31,7 +31,7 @@ app.use(hpp()); // Prevent HTTP Parameter Pollution
 // Rate Limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 1000, // Limit each IP to 1000 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: 'Too many requests from this IP, please try again later.'
